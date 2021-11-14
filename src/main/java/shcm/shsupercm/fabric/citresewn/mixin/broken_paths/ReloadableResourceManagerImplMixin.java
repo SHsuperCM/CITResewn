de03174eaeaed79c98b26c0abdf4674cdbb79589
SHsuperCM <shsupercm@gmail.com>
2021-11-14 16:11:29 +0200

    Fixed broken paths log not showing when warns are muted
    Fixes #66

--- src/main/java/shcm/shsupercm/fabric/citresewn/mixin/broken_paths/ReloadableResourceManagerImplMixin.java
+++ src/main/java/shcm/shsupercm/fabric/citresewn/mixin/broken_paths/ReloadableResourceManagerImplMixin.java
@@ -24,7 +24,7 @@ public class ReloadableResourceManagerImplMixin {
     @Inject(method = "reload", at = @At("RETURN"))
     public void onReload(Executor prepareExecutor, Executor applyExecutor, CompletableFuture<Unit> initialStage, List<ResourcePack> packs, CallbackInfoReturnable<ResourceReload> cir) {
         if (CITResewn.INSTANCE.processingBrokenPaths = this.type == ResourceType.CLIENT_RESOURCES) {
-            CITResewn.logWarnLoading("Caution! Broken paths is enabled!");
+            CITResewn.LOG.error("[citresewn] Caution! Broken paths is enabled!");
             cir.getReturnValue().whenComplete().thenRun(() -> CITResewn.INSTANCE.processingBrokenPaths = false);
         }
     }
