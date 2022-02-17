9db8d1eafb861f6512e7ae0d8584b5ace676cc3c
SHsuperCM <shsupercm@gmail.com>
2022-02-17 17:15:26 +0200

    Implemented enchantments and enchantmentLevels conditions

---
+++ defaults/src/main/java/shcm/shsupercm/fabric/citresewn/defaults/cit/conditions/ConditionEnchantments.java
@@ -0,0 +1,59 @@
+package shcm.shsupercm.fabric.citresewn.defaults.cit.conditions;
+
+import io.shcm.shsupercm.fabric.fletchingtable.api.Entrypoint;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import shcm.shsupercm.fabric.citresewn.api.CITConditionContainer;
+import shcm.shsupercm.fabric.citresewn.cit.CITCondition;
+import shcm.shsupercm.fabric.citresewn.cit.CITContext;
+import shcm.shsupercm.fabric.citresewn.cit.builtin.conditions.IdentifierCondition;
+import shcm.shsupercm.fabric.citresewn.cit.builtin.conditions.ListCondition;
+import shcm.shsupercm.fabric.citresewn.ex.CITParsingException;
+import shcm.shsupercm.fabric.citresewn.pack.format.PropertyGroup;
+import shcm.shsupercm.fabric.citresewn.pack.format.PropertyValue;
+
+import java.util.Set;
+
+public class ConditionEnchantments extends ListCondition<ConditionEnchantments.EnchantmentCondition> {
+    @Entrypoint(CITConditionContainer.ENTRYPOINT)
+    public static final CITConditionContainer<ConditionEnchantments> CONTAINER = new CITConditionContainer<>(ConditionEnchantments.class, ConditionEnchantments::new,
+            "enchantments", "enchantmentIDs");
+
+    public ConditionEnchantments() {
+        super(EnchantmentCondition.class, EnchantmentCondition::new);
+    }
+
+    public Identifier[] getEnchantments() {
+        Identifier[] enchantments = new Identifier[this.conditions.length];
+
+        for (int i = 0; i < this.conditions.length; i++)
+            enchantments[i] = this.conditions[i].getValue(null);
+
+        return enchantments;
+    }
+
+    @Override
+    public Set<Class<? extends CITCondition>> siblingConditions() {
+        return Set.of(ConditionEnchantmentLevels.class);
+    }
+
+    protected static class EnchantmentCondition extends IdentifierCondition {
+        @Override
+        public void load(PropertyValue value, PropertyGroup properties) throws CITParsingException {
+            super.load(value, properties);
+
+            if (!Registry.ENCHANTMENT.containsId(this.value))
+                warn(this.value + " is not in the enchantment registry", value, properties);
+        }
+
+        @Override
+        public boolean test(CITContext context) {
+            return context.enchantments().containsKey(this.value);
+        }
+
+        @Override
+        protected Identifier getValue(CITContext context) {
+            return this.value;
+        }
+    }
+}
--- src/main/java/shcm/shsupercm/fabric/citresewn/cit/CITContext.java
+++ src/main/java/shcm/shsupercm/fabric/citresewn/cit/CITContext.java
@@ -1,10 +1,18 @@
 package shcm.shsupercm.fabric.citresewn.cit;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.EnchantedBookItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class CITContext {
@@ -12,10 +20,21 @@ public class CITContext {
     public final World world;
     public final LivingEntity entity;
 
+    private Map<Identifier, Integer> enchantments = null;
+
     public CITContext(ItemStack stack, World world, LivingEntity entity) {
         this.stack = stack;
         this.world = world == null ? MinecraftClient.getInstance().world : world;
         this.entity = entity;
+    }
+
+    public Map<Identifier, Integer> enchantments() {
+        if (this.enchantments == null) {
+            this.enchantments = new LinkedHashMap<>();
+            for (NbtElement nbtElement : stack.isOf(Items.ENCHANTED_BOOK) ? EnchantedBookItem.getEnchantmentNbt(stack) : stack.getEnchantments())
+                this.enchantments.put(EnchantmentHelper.getIdFromNbt((NbtCompound) nbtElement), EnchantmentHelper.getLevelFromNbt((NbtCompound) nbtElement));
+        }
+        return this.enchantments;
     }
 
     @Override
