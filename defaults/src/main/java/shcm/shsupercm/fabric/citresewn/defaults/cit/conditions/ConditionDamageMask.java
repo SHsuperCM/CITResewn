1ec38a503f3b88c9de1e29319548b5a6e6098707
SHsuperCM <shsupercm@gmail.com>
2022-02-17 12:55:24 +0200

    Implemented conditions stackSize, damage, damageMask

---
+++ defaults/src/main/java/shcm/shsupercm/fabric/citresewn/defaults/cit/conditions/ConditionDamageMask.java
@@ -0,0 +1,30 @@
+package shcm.shsupercm.fabric.citresewn.defaults.cit.conditions;
+
+import io.shcm.shsupercm.fabric.fletchingtable.api.Entrypoint;
+import shcm.shsupercm.fabric.citresewn.api.CITConditionContainer;
+import shcm.shsupercm.fabric.citresewn.cit.CITContext;
+import shcm.shsupercm.fabric.citresewn.cit.builtin.conditions.IntegerCondition;
+
+public class ConditionDamageMask extends IntegerCondition {
+    @Entrypoint(CITConditionContainer.ENTRYPOINT)
+    public static final CITConditionContainer<ConditionDamageMask> CONTAINER = new CITConditionContainer<>(ConditionDamageMask.class, ConditionDamageMask::new,
+            "damageMask");
+
+    public ConditionDamageMask() {
+        super(false, false, false);
+    }
+
+    @Override
+    protected int getValue(CITContext context) {
+        return 0;
+    }
+
+    @Override
+    public boolean test(CITContext context) {
+        return true;
+    }
+
+    public int getMask() {
+        return this.min;
+    }
+}
