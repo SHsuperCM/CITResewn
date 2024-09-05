bc65042d74b560bc6020f41cedc43273a672ff28
SHsuperCM <shsupercm@gmail.com>
2024-09-05 09:43:16 +0300

    Started components condition

---
+++ defaults/src/main/java/shcm/shsupercm/fabric/citresewn/defaults/cit/conditions/ConditionComponents.java
@@ -0,0 +1,26 @@
+package shcm.shsupercm.fabric.citresewn.defaults.cit.conditions;
+
+import io.shcm.shsupercm.fabric.fletchingtable.api.Entrypoint;
+import shcm.shsupercm.fabric.citresewn.api.CITConditionContainer;
+import shcm.shsupercm.fabric.citresewn.cit.CITCondition;
+import shcm.shsupercm.fabric.citresewn.cit.CITContext;
+import shcm.shsupercm.fabric.citresewn.cit.CITParsingException;
+import shcm.shsupercm.fabric.citresewn.pack.format.PropertyGroup;
+import shcm.shsupercm.fabric.citresewn.pack.format.PropertyKey;
+import shcm.shsupercm.fabric.citresewn.pack.format.PropertyValue;
+
+public class ConditionComponents extends CITCondition {
+    /*?>=1.21 {?*/@Entrypoint(CITConditionContainer.ENTRYPOINT)/*?}?*/
+    public static final CITConditionContainer<ConditionComponents> CONTAINER = new CITConditionContainer<>(ConditionComponents.class, ConditionComponents::new,
+            "components", "component", "nbt");
+
+    @Override
+    public void load(PropertyKey key, PropertyValue value, PropertyGroup properties) throws CITParsingException {
+        
+    }
+
+    @Override
+    public boolean test(CITContext context) {
+        return false;
+    }
+}
