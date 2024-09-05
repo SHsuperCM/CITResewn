f9384a387c4c30763a55ace69a711e9d9f186683
SHsuperCM <shsupercm@gmail.com>
2024-09-05 10:40:11 +0300

    Added legacy 1.21 support for nbt name and lore

--- defaults/src/main/java/shcm/shsupercm/fabric/citresewn/defaults/cit/conditions/ConditionComponents.java
+++ defaults/src/main/java/shcm/shsupercm/fabric/citresewn/defaults/cit/conditions/ConditionComponents.java
@@ -1,6 +1,7 @@
 package shcm.shsupercm.fabric.citresewn.defaults.cit.conditions;
 
 import io.shcm.shsupercm.fabric.fletchingtable.api.Entrypoint;
+import shcm.shsupercm.fabric.citresewn.CITResewn;
 import shcm.shsupercm.fabric.citresewn.api.CITConditionContainer;
 import shcm.shsupercm.fabric.citresewn.cit.CITCondition;
 import shcm.shsupercm.fabric.citresewn.cit.CITContext;
@@ -16,7 +17,17 @@ public class ConditionComponents extends CITCondition {
 
     @Override
     public void load(PropertyKey key, PropertyValue value, PropertyGroup properties) throws CITParsingException {
-        
+        if (key.path().equals("nbt")) {
+            if (value.keyMetadata().startsWith("display.Name")) {
+                value = new PropertyValue("minecraft:custom_name" + value.keyMetadata().substring("display.Name".length()), value.value(), value.separator(), value.position(), value.propertiesIdentifier(), value.packName());
+                CITResewn.logWarnLoading(properties.messageWithDescriptorOf("Using legacy nbt.display.Name", value.position()));
+            } else if (value.keyMetadata().startsWith("display.Lore")) {
+                value = new PropertyValue("minecraft:lore" + value.keyMetadata().substring("display.Lore".length()), value.value(), value.separator(), value.position(), value.propertiesIdentifier(), value.packName());
+                CITResewn.logWarnLoading(properties.messageWithDescriptorOf("Using legacy nbt.display.Lore", value.position()));
+            } else
+                throw new CITParsingException("NBT condition is not supported since 1.21", properties, value.position());
+        }
+
     }
 
     @Override
