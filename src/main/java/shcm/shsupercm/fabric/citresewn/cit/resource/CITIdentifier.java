526341198a5b06f9b4192c957ddfd67c639fd636
SHsuperCM <shsupercm@gmail.com>
2024-10-21 14:41:12 +0300

    Fixed fapi version for 1.19.4

--- src/main/java/shcm/shsupercm/fabric/citresewn/cit/resource/CITIdentifier.java
+++ src/main/java/shcm/shsupercm/fabric/citresewn/cit/resource/CITIdentifier.java
@@ -2,7 +2,6 @@ package shcm.shsupercm.fabric.citresewn.cit.resource;
 
 import net.minecraft.resource.Resource;
 import net.minecraft.resource.ResourcePack;
-import net.minecraft.resource.ResourcePackInfo;
 import net.minecraft.util.Identifier;
 
 public record CITIdentifier(ResourcePack pack, String root, Identifier path) {
