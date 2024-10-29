e4072fc31d83dd6d8e8d9505c26cd81b225182fe
SHsuperCM <shsupercm@gmail.com>
2024-10-29 09:21:50 +0200

    More model access api

--- src/main/java/shcm/shsupercm/fabric/citresewn/cit/model/CITModelsAccess.java
+++ src/main/java/shcm/shsupercm/fabric/citresewn/cit/model/CITModelsAccess.java
@@ -7,32 +7,53 @@ import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 import shcm.shsupercm.fabric.citresewn.cit.resource.CITResources;
 
+import java.util.*;
 import java.util.function.Consumer;
 
 public class CITModelsAccess {
     private final ResourceManager manager;
+    private final Set<RequestEntry> modelRequests = new HashSet<>();
 
     public CITModelsAccess(ResourceManager manager) {
         this.manager = manager;
     }
 
-    public void requestModel(Identifier path, Consumer<BakedModel> bakedModelReceiver) {
-        requestModel(path, ModelFinder.DEFAULT, bakedModelReceiver);
-    }
-
-    public void requestModel(Identifier path, ModelFinder modelResolver, Consumer<BakedModel> modelReceiver) {
-
-    }
-
     public CITResources.CITModels build() {
-        return new CITResources.CITModels();
+        CITResources.CITModels models = new CITResources.CITModels(new HashSet<>(), new IdentityHashMap<>(), new IdentityHashMap<>());
+
+        int generatedCount = 0;
+
+        for (RequestEntry entry : modelRequests) {
+            final Identifier modelPath = entry.modelPath();
+            final ModelFinder modelFinder = entry.modelFinder();
+
+            Identifier modelIdentifier = Identifier.of("citresewn", "generated/" + generatedCount++ + "/" + modelPath.getNamespace() + "/" + modelPath.getPath());
+            models.extraModels().add(modelIdentifier);
+
+            models.modelResolvers().put(modelIdentifier, resolverContext ->
+                    modelFinder.resolve(modelPath, manager, resolverContext));
+
+            models.bakedModelReceivers().put(modelIdentifier, entry.bakedModelReceiver());
+        }
+
+        return models;
+    }
+
+    public void requestModel(Identifier modelPath, Consumer<BakedModel> bakedModelReceiver) {
+        requestModel(modelPath, ModelFinder.DEFAULT, bakedModelReceiver);
+    }
+
+    public void requestModel(Identifier modelPath, ModelFinder modelFinder, Consumer<BakedModel> modelReceiver) {
+        modelRequests.add(new RequestEntry(modelPath, modelFinder, modelReceiver));
     }
 
     public interface ModelFinder {
-        ModelFinder DEFAULT = context -> {
+        ModelFinder DEFAULT = (path, resourceManager, resolverContext) -> {
             return null;
         };
 
-        UnbakedModel resolve(ModelResolver.Context context);
+        UnbakedModel resolve(Identifier path, ResourceManager resourceManager, ModelResolver.Context resolverContext);
     }
+
+    private record RequestEntry(Identifier modelPath, ModelFinder modelFinder, Consumer<BakedModel> bakedModelReceiver) { }
 }
--- src/main/java/shcm/shsupercm/fabric/citresewn/cit/resource/CITResources.java
+++ src/main/java/shcm/shsupercm/fabric/citresewn/cit/resource/CITResources.java
@@ -1,8 +1,12 @@
 package shcm.shsupercm.fabric.citresewn.cit.resource;
 
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelResolver;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.util.Identifier;
 import shcm.shsupercm.fabric.citresewn.cit.CIT;
 
-import java.util.Map;
+import java.util.*;
+import java.util.function.Consumer;
 
 public record CITResources(
         CITData citData,
@@ -16,7 +20,10 @@ public record CITResources(
 
     }
 
-    public record CITModels() {
-        public static final CITModels EMPTY = new CITModels();
+    public record CITModels(Set<Identifier> extraModels,
+                            IdentityHashMap<Identifier, ModelResolver> modelResolvers,
+                            IdentityHashMap<Identifier, Consumer<BakedModel>> bakedModelReceivers) {
+        public static final CITModels EMPTY = new CITModels(Set.of(), new IdentityHashMap<>(), new IdentityHashMap<>());
+
     }
 }
