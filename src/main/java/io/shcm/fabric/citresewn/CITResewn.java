34ee4896cce30350bbfdeebff698a46979ac5a64
SHsuperCM <shsupercm@gmail.com>
2024-12-05 12:58:07 +0200

    Started setup of client component changes

--- src/main/java/io/shcm/fabric/citresewn/CITResewn.java
+++ src/main/java/io/shcm/fabric/citresewn/CITResewn.java
@@ -1,4 +1,9 @@
 package io.shcm.fabric.citresewn;
 
+import io.shcm.shsupercm.fabric.fletchingtable.api.Entrypoint;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class CITResewn {
+    public static final Logger LOG = LogManager.getLogger("citresewn");
 }
---
+++ src/main/java/io/shcm/fabric/citresewn/client_component_changes/ClientComponentChanges.java
@@ -0,0 +1,43 @@
+package io.shcm.fabric.citresewn.client_component_changes;
+
+import it.unimi.dsi.fastutil.objects.Reference2ObjectArrayMap;
+import it.unimi.dsi.fastutil.objects.Reference2ObjectMap;
+import net.minecraft.component.ComponentChanges;
+import net.minecraft.component.ComponentHolder;
+import net.minecraft.component.ComponentType;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class ClientComponentChanges {
+    public static final ClientComponentChanges EMPTY = new ClientComponentChanges(new Reference2ObjectArrayMap<>()) {
+        @Override
+        public <T> T getOrDefault(ComponentType<? extends T> type, T original) {
+            return original;
+        }
+    };
+
+    private final Reference2ObjectMap<ComponentType<?>, Optional<?>> changedComponents;
+
+    public ClientComponentChanges(Reference2ObjectMap<ComponentType<?>, Optional<?>> changedComponents) {
+        this.changedComponents = changedComponents;
+    }
+
+    public static <T extends ComponentHolder> ClientComponentChanges fromHolder(T holder) {
+        if (holder instanceof ItemStack stack)
+            return new ClientComponentChanges(new Reference2ObjectArrayMap<>());
+
+        return new ClientComponentChanges(new Reference2ObjectArrayMap<>());
+    }
+
+    public <T> T getOrDefault(ComponentType<? extends T> type, T original) {
+        Optional<?> clientComponent = this.changedComponents.get(type);
+        if (clientComponent != null)
+            return (T) clientComponent.orElse(null);
+        return original;
+    }
+}
