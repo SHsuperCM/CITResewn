463922adc368b4010f2c53df9d9c43c8eacf6056
SHsuperCM <shsupercm@gmail.com>
2024-12-12 12:24:28 +0200

    Renamed CITEffect → CITModifier

--- src/main/java/io/shcm/fabric/citresewn/pack/cit/effect/CITEffect.java
+++
@@ -1,7 +0,0 @@
-package io.shcm.fabric.citresewn.pack.cit.effect;
-
-import com.mojang.serialization.Codec;
-
-public class CITEffect {
-    public static final Codec<CITEffect> CODEC = Codec.unit(new CITEffect());
-}
---
+++ src/main/java/io/shcm/fabric/citresewn/pack/cit/effect/CITModifier.java
@@ -0,0 +1,7 @@
+package io.shcm.fabric.citresewn.pack.cit.effect;
+
+import com.mojang.serialization.Codec;
+
+public class CITModifier {
+    public static final Codec<CITModifier> CODEC = Codec.unit(new CITModifier());
+}
